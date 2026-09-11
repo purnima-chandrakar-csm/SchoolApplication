53fe3e4bfa96232b9bcb36496bac3be14048a3fd
AnshuCsm <anshu.singh@csm.tech>
2026-09-11 11:56:22 +0530

    Chnaged HomeEF By Anshu Singh

--- HomeEF/Teacher.cs
+++ HomeEF/Teacher.cs
@@ -12,7 +12,6 @@ namespace HomeEF
         public string TeacherName { get; set; } = string.Empty;
         public string TeacherEmail { get; set; } = string.Empty;
         public string TeacherPhone { get; set; } = string.Empty;
-        public string TeacherPhoto { get; set; } = string.Empty;
     }
 
    
